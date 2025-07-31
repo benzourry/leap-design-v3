cea9a00c70ea068f252354962304700c86c52cde
Razif Baital <benzourry@gmail.com>
2025-07-31 22:21:09 +0800

    Fix logic to set default value. - default value is not set in effect(), but through override of writeValue()
    Fix pushDismissed() to use signal() instead of computed()

--- src/app/run/start/start.component.ts
+++ src/app/run/start/start.component.ts
@@ -76,7 +76,7 @@ export class StartComponent implements OnInit, OnDestroy {
   sidebarActive = signal<boolean>(false);
   // frameless = signal<boolean>(false);
   frameless = computed(() => (getQuery('noframe') || localStorage.getItem('noframe')) === 'true');
-  pushDismissed = computed(() => localStorage.getItem('pushDismissed') === '1');
+  pushDismissed = signal(localStorage.getItem('pushDismissed') === '1');
   maintenance = computed(() => {
     const app = this.app();
     const path = this.getPath();
@@ -219,7 +219,7 @@ export class StartComponent implements OnInit, OnDestroy {
 
   dismissPush() {
     localStorage.setItem("pushDismissed", "1");
-    // this.pushDismissed.set(true);
+    this.pushDismissed.set(true);
   }
 
   toggleNav(index: number): void {
