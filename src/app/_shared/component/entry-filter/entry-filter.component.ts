5e6aa2cdaa05d60639f1fc6d91325b5e51408a6e
Razif Baital <benzourry@gmail.com>
2026-04-30 12:03:53 +0800

    - Form - New field-edit (to replace field-edit-b) - Fix start (home link on title) - Revamp View - Optimize styles.scss

--- src/app/_shared/component/entry-filter/entry-filter.component.ts
+++ src/app/_shared/component/entry-filter/entry-filter.component.ts
@@ -7,83 +7,88 @@ import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-entry-filter',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    templateUrl: './entry-filter.component.html',
-    styleUrls: ['./entry-filter.component.scss'],
-    imports: [FormsModule, NgbInputDatepicker, FaIconComponent, FilterPipe]
+  selector: 'app-entry-filter',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './entry-filter.component.html',
+  styleUrls: ['./entry-filter.component.scss'],
+  imports: [FormsModule, NgbInputDatepicker, FaIconComponent, FilterPipe]
 })
 export class EntryFilterComponent {
 
   filterField: string = "";
-
   presetFilters = model<any>({});
-
   filters = model<any[]>([]);
-
   formHolder = input<any>();
-
   sectionItems = input<any>({});
-
+  lookup = input<any>({});
+  
   hasFocus: any = {};
 
-  lookup = input<any>({})
+  excludedTypes = ['static', 'file', 'imagePreview', 'btn', 'dataset', 'screen', 'map'];
+  noDynamicTypes = ['text', 'textarea', 'eval', 'qr'];
 
-  getPrefix = (fm, section) => {
-    var mapFm = { 'data': '$', 'prev': '$prev$', 'approval': '$$' };
-    var tier = this.getTierFromSection(section && section.id, this.formHolder()['data']);
-    if (section && section.type == 'approval') {
-      if (tier) {
-        return '$$.' + tier.id;
+  getPrefix(fm: string, section: any): string {
+    const mapFm: Record<string, string> = { 'data': '$', 'prev': '$prev$', 'approval': '$$' };
+    const tier = this.getTierFromSection(section?.id, this.formHolder()['data']);
+    
+    if (section?.type === 'approval') {
+      // FIXED: Removed the rogue leading dot. Now correctly yields $$.123
+      return tier ? `$$.${tier.id}` : mapFm[fm];
+    } else if (section?.type === 'list') {
+      return `${mapFm[fm]}.${section.code}*`;
+    }
+    return mapFm[fm];
+  }
+
+  clearPreset(keys: string[]) {
+    this.presetFilters.update(pf => {
+      const newPf = { ...pf }; 
+      keys.forEach(k => delete newPf[k]);
+      return newPf;
+    });
+  }
+
+  getTierFromSection = (sectionId: any, form: any) => form?.tiers.find((t: any) => t.section?.id == sectionId);
+
+  toggleItem(f: any, root: string, formId: any, type: string, prefix: string) {
+    this.filters.update(filters => {
+      const exists = filters.some(i => i.code === f.code && i.root === root);
+      if (exists) {
+        return filters.filter(i => !(i.code === f.code && i.root === root));
       } else {
-        return mapFm[fm];
+        return [...filters, {
+          code: f.code, label: f.label, sortOrder: filters.length,
+          root: root, type: type, prefix: prefix, formId: formId
+        }];
       }
-    } else if (section && section.type == 'list') {
-      return mapFm[fm] + '.' + section.code + '*';
-    } else {
-      return mapFm[fm];
+    });
+  }
+
+  checkItem(f: any, root: string): boolean {
+    return this.filters().some(i => f.code === i.code && i.root === root);
+  }
+
+  getBindingPath(field: any, prefixedField: string): string {
+    const multi = field.subType === 'multiple' ? '*' : '';
+    switch (field.type) {
+      case 'select':
+      case 'radio': 
+        return `${prefixedField}${multi}.code`;
+      case 'checkboxOption': 
+        return `${prefixedField}*.code`;
+      case 'modelPicker': 
+        return `${prefixedField}${multi}.${field.bindLabel}`;
+      default: 
+        return prefixedField;
     }
   }
 
-
-  clearPreset(key) {
-    key.forEach(k => {
-      this.presetFilters.update(pf=>{
-        delete pf[k];
-        return pf;
-      })
-    })
-  }
-
-  getTierFromSection = (sectionId, form) => form && form.tiers.filter(t => t.section && t.section.id == sectionId)[0];
-
-  toggleItem(f, root, formId, type, prefix) {
-    let size = this.filters().filter(i => i.code == f.code && i.root == root).length;
-
-    // Is currently selected
-    if (size > 0) {
-      this.filters.update(filters => filters.filter(i => !(i.code == f.code && i.root == root)));
-    } else {
-      this.filters.update(filters => [...filters, {
-        code: f.code,
-        label: f.label,
-        sortOrder: this.filters().length,
-        root: root,
-        type: type,
-        prefix: prefix,
-        formId: formId
-      }])
+  getClearKeys(field: any, prefixedField: string): string[] {
+    if (['scaleTo5', 'scaleTo10', 'scale', 'number', 'date'].includes(field.type)) {
+      return [`${prefixedField}~from`, `${prefixedField}~to`];
     }
-  }
-
-  checkItem(f, root) {
-    return this.filters().filter(i => f.code == i.code && i.root == root).length > 0;
-  }
-
-  getFilterPath(fm, section,f){
-    return this.getPrefix(fm,section)+'.'+f.code+(this.formHolder()[fm].items[f.code].subType=='multiple'?'*':'')+'.code';
+    return [this.getBindingPath(field, prefixedField)];
   }
 
   getAsList = splitAsList;
-
 }
