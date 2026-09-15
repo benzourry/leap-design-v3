e623c6a399c041ed61ca18a1c74c25fdbebf44b5
Razif Baital <benzourry@gmail.com>
2026-09-16 00:32:23 +0800

    New pinned bottom nav
    Improve UI
    
    Tab using mask-image instead of background glow effect

--- src/app/run/start-15-09-2026-2116/start.component.ts
+++ src/app/run/start-15-09-2026-2116/start.component.ts
@@ -500,7 +500,7 @@ export class StartComponent implements OnInit, OnDestroy {
       updatedPreGroup[group.id] = this.preCheck(group);
 
       // FIX: Use group.id instead of index, and ensure bottom navs don't steal the active state
-      if (!firstActiveSet && updatedPreGroup[group.id]) {
+      if (!firstActiveSet && updatedPreGroup[group.id]) {  //&& group.x?.type !== 'bottom'
         firstActiveSet = true;
         updatedNavToggle[group.id] = true;
       }
